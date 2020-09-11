f7d6aa23d6d3e2372a3fdfae35ac685ef9d0e368
min <1736s@naver.com>
2020-09-12 00:25:45 +0900

    move jet

--- Game1945/src/application/Controller.java
+++ Game1945/src/application/Controller.java
@@ -3,9 +3,12 @@ package application;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
 
 public class Controller implements Initializable{
@@ -15,25 +18,27 @@ public class Controller implements Initializable{
 	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		jet.setOnKeyTyped(event-> {
-			switch (event.getCode()) {
-				case UP :
-					jet.setLayoutY(jet.getLayoutY()-5);
-					break;
-				case RIGHT :
-					jet.setLayoutX(jet.getLayoutX() + 5);
-					break;
-				case DOWN :
-					jet.setLayoutY(jet.getLayoutY()+5);
-					break;
-				case LEFT :
-					jet.setLayoutX(jet.getLayoutX()-5);
-					break;
-				default:
-					break;
+		jet.addEventHandler(KeyEvent.KEY_PRESSED, new EventHandler<KeyEvent>() {
+			@Override
+			public void handle(KeyEvent event) {
+				if (event.getCode() == KeyCode.UP) {
+					jet.setLayoutY(jet.getLayoutY()-10);
+				} else if (event.getCode() == KeyCode.RIGHT) {
+					jet.setLayoutX(jet.getLayoutX()+10);
+				} else if (event.getCode() == KeyCode.LEFT) {
+					jet.setLayoutX(jet.getLayoutX()-10);
+				}  else if (event.getCode() == KeyCode.DOWN) {
+					jet.setLayoutY(jet.getLayoutY()+10);
+				}  
+				if (event.getCode() == KeyCode.SPACE) {
+					Thread thread = new Thread( ()-> {
+						
+					});
+					thread.start();
+				}
 			}
-			
 		});
+		
 		
 	}
 }
