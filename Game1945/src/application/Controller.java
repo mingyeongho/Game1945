eaee4f1252276933f9f8466b2c34ad78c0097a45
min <1736s@naver.com>
2020-09-13 03:53:54 +0900

    add circle, meteor

--- Game1945/src/application/Controller.java
+++ Game1945/src/application/Controller.java
@@ -1,10 +1,9 @@
 package application;
 
 import java.net.URL;
-import java.util.Random;
 import java.util.ResourceBundle;
+import java.util.Vector;
 
-import javafx.application.Platform;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -12,15 +11,18 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 
 public class Controller implements Initializable{
 
+	@FXML AnchorPane pane;
 	@FXML ImageView jet;
+
 	
-	Random random = new Random();
-	Image meteor; 
-	Circle circle = new Circle(1); // bullet
+	
+	
 	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
@@ -41,34 +43,50 @@ public class Controller implements Initializable{
 				
 				// shootingThread
 				if (event.getCode() == KeyCode.SPACE) {
-					Platform.runLater(()-> {
-						circle.setLayoutX(jet.getLayoutX() + jet.getFitWidth()/2);
-						circle.setLayoutY(jet.getLayoutY() - 10);
-						Thread shootingThread = new Thread(()-> {
-							while(true) {
-								circle.setLayoutY(circle.getLayoutY() - 10);
-								try {
-									Thread.sleep(500);
-								} catch(Exception e) {
-									
-								}
-								if (circle.getLayoutY() == 0) {
-									break;
-								}
+					Circle circle = new Circle(jet.getLayoutX() + 28, jet.getLayoutY() - 10, 5);
+					circle.setFill(Color.RED);
+					pane.getChildren().add(circle);
+					Thread shootingThread = new Thread(()-> {
+						while(true) {
+							circle.setLayoutY(circle.getLayoutY()-10);
+							try {
+								Thread.sleep(50);
+							} catch(Exception e) {
+								
 							}
-						});
-						shootingThread.start();
+							if (circle.getLayoutY() == 0) {
+								pane.getChildren().remove(circle);
+								break;
+							}
+						}
 					});
+					shootingThread.setDaemon(true);
+					shootingThread.start();
 				}
 			}
 		});
 		
-		// 운석이 떨어지는 쓰레드
-//		Thread meteorThread = new Thread(()->{
-//			
-//			
-//		});
-//		meteorThread.start();
+//		 운석이 떨어지는 쓰레드
+		Thread meteorThread = new Thread(()->{
+			ImageView imageview = new ImageView(new Image("file:///C:/Users/min%20gyeong%20ho/git/Game1945/Game1945/src/images/meteor.png"));
+			imageview.setFitWidth(50);
+			imageview.setFitHeight(50);
+			imageview.setLayoutX(100);
+			imageview.setLayoutY(0);
+			pane.getChildren().add(imageview);
+			while(true) {
+				imageview.setLayoutY(imageview.getLayoutY()+10);
+				try {
+					Thread.sleep(100);
+				} catch(Exception e) {
+					
+				}
+				if (imageview.getLayoutY() == 0) {
+					pane.getChildren().remove(imageview);
+				}
+			}
+		});
+		meteorThread.start();
 		
 		// 검은 배경에 흰색 점이 떨어지는 쓰레드
 //		Thread starThread = new Thread( ()-> {
@@ -76,5 +94,6 @@ public class Controller implements Initializable{
 //			
 //		});
 //		starThread.start();
+		
 	}
 }
